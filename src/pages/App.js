d7767b2aba05022890b9bb3e0fdc28fac38b2968
Roman Zakrevskyi <r.zakrevskyi@weplay.tv>
2022-05-24 20:58:17 +0300

    Initial

---
+++ src/pages/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Redux-saga tutorial: lesson #3</h1>
+}
+
+export default App
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
+
+export const history = createBrowserHistory()
+
+const initial = {}
+
+export function appReducer(state = initial, action) {
+  return state
+}
+
+const rootReducer = combineReducers({
+  app: appReducer,
+  router: connectRouter(history),
+})
+
+export default rootReducer
